cfc9726d4af75c542b0e71f447595341b0f624b3
Henry Galvez <hgalvezq@unsa.edu.pe>
2025-10-01 18:49:34 -0500

    Resolucion cambiada a mobile 9:16

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', '540')
+Config.set('graphics', 'height', '960')
+
 from kivy.app import App
 from game.screen import GameScreen
+
 
 class MyGameApp(App):
     def build(self):
         return GameScreen()
+
 
 if __name__ == "__main__":
     MyGameApp().run()
