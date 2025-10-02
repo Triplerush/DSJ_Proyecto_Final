7d34ed5973dff0a15e1002c0776cd6e6e121843c
HenryGalvezQ <hgalVezq@unsa.edu.pe>
2025-10-02 08:45:25 -0500

    Musica de fondo agregada

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from game.screen import GameScreen
 from game.menu import MainMenu, LevelScreen, InstructionsScreen
-
+from kivy.core.audio import SoundLoader
 
 class RootWidget(Widget):
     """Widget raíz que contiene todas las pantallas"""
@@ -18,10 +18,17 @@ class MyGameApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.root_widget = None
-    
+            # --- 2. CARGAR Y CONFIGURAR LA MÚSICA ---
+        self.music = SoundLoader.load('images/musica.mp3')
+        if self.music:
+            self.music.loop = True
+            self.music.volume = 0.5 # Opcional: ajusta el volumen (0.0 a 1.0)
     def build(self):
         # Crear el widget raíz
         self.root_widget = RootWidget()
+                # --- 3. REPRODUCIR LA MÚSICA --- 
+        if self.music:
+            self.music.play()
         # Mostrar el menú principal al iniciar
         self.show_main_menu()
         return self.root_widget
